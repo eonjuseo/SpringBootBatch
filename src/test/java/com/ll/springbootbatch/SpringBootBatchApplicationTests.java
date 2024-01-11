6f7f9c174c644b3418ccb222e0a285e5cfa16953
eonjuseo <ejooss@chungbuk.ac.kr>
2024-01-11 13:27:07 +0900

    work

---
+++ src/test/java/com/ll/springbootbatch/SpringBootBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.springbootbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
